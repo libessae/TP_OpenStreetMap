ebf0ee361ac55eef668f8414761e82493283114d
Emilie Libessart <libessae@ensipc80.imag.fr>
2014-01-29 13:06:04 +0100

    question 11

--- src/JavaGIS.java
+++ src/JavaGIS.java
@@ -3,10 +3,14 @@ import geoexplorer.gui.GraphicalPrimitive;
 import geoexplorer.gui.MapPanel;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.awt.Color;
 
 import org.postgis.*;
+
 import database.Utils;
 
 public class JavaGIS {
@@ -38,19 +42,37 @@ public class JavaGIS {
 			
 			// Graphique
 			
-			MapPanel mp = new MapPanel(916074.4, 6453841.3, 15000);
+			Map <String, Color> couleur = new HashMap<String,Color>(); 
+			couleur.put("motorway", Color.blue);
+			couleur.put("trunk", Color.green);
+			couleur.put("primary", Color.red);
+			couleur.put("secondary", new Color(237,127,16));
+			couleur.put("tertiary", new Color(255,255,20));
+			couleur.put("unclassified", new Color(88,41,0));
+			couleur.put("residential", Color.lightGray);
+			couleur.put("service", Color.pink);
+
+			
+			MapPanel mp = new MapPanel(915000, 6456500, 15000);
 			
 			java.sql.Connection conn = Utils.getConnection();
 			PreparedStatement stmt;
 
 			try {
-				stmt = conn
-						.prepareStatement("SELECT ST_Transform(linestring, 2154) FROM ways WHERE tags->'highway' LIKE '%' AND ST_Intersects(linestring, ST_Setsrid(ST_GeomFromText('Polygon((5.8 45.1, 5.8 45.2, 5.7 45.2, 5.7 45.1, 5.8 45.1))'),4326))");
+			/*	stmt = conn
+						.prepareStatement("SELECT ST_Transform(linestring, 2154), tags->'highway' FROM ways WHERE tags->'highway' LIKE '%' AND ST_Intersects(linestring, ST_Setsrid(ST_GeomFromText('Polygon((5.8 45.1, 5.8 45.2, 5.7 45.2, 5.7 45.1, 5.8 45.1))'),4326))");
 
 				ResultSet res = stmt.executeQuery();
+				geoexplorer.gui.LineString linestring;
 				while (res.next()) {
-					geoexplorer.gui.LineString linestring = new geoexplorer.gui.LineString();
-					System.out.println("linestring = " + ((PGgeometry) res.getObject(1)).getGeometry());
+					String highway = res.getString(2);
+					Color color = couleur.get(highway);
+					if ( color != null) {
+						linestring = new geoexplorer.gui.LineString(color);
+					}else{
+						linestring = new geoexplorer.gui.LineString();
+					}
+					
 					PGgeometry geom = (PGgeometry)res.getObject(1);
 					LineString ls = (LineString)geom.getGeometry();
 					for( int p = 0; p < ls.numPoints(); p++ )
@@ -63,16 +85,85 @@ public class JavaGIS {
 
 				}
 				res.close();
+				
+				stmt = conn
+						.prepareStatement("SELECT ST_Transform(bbox, 2154) FROM ways WHERE tags->'building' LIKE '%' AND ST_Intersects(linestring, ST_Setsrid(ST_GeomFromText('Polygon((5.8 45.1, 5.8 45.2, 5.7 45.2, 5.7 45.1, 5.8 45.1))'),4326))");
+
+				res = stmt.executeQuery();
+				geoexplorer.gui.Polygon polygone;
+				while (res.next()) {
+					polygone = new geoexplorer.gui.Polygon();
+					
+					PGgeometry geom = (PGgeometry)res.getObject(1);
+					Polygon pol = (Polygon)geom.getGeometry();
+					for( int p = 0; p < pol.numPoints(); p++ )
+					{
+						Point pt = pol.getPoint(p);
+						polygone.addPoint(new geoexplorer.gui.Point(pt.x, pt.y));
+					}
+					mp.addPrimitive(polygone);
+
+
+				}*/
+								
+				stmt = conn
+						.prepareStatement("SELECT q.quartier, q.the_geom, COUNT(n) FROM quartier q, nodes n WHERE n.tags->'shop'='bakery' AND ST_Intersects(ST_Transform(q.the_geom, 4326), n.geom) GROUP BY q.quartier, q.the_geom ORDER BY COUNT(n) DESC");
+
+				ResultSet res = stmt.executeQuery();
+				geoexplorer.gui.Polygon polygone;
+				while (res.next()) {
+					Color couleur_quartier;
+					int nb_boulangeries = res.getInt(3);
+					if (nb_boulangeries < 5){
+						couleur_quartier = Color.yellow;
+					}else if (nb_boulangeries < 10){
+						couleur_quartier = Color.orange;
+					}else{
+						couleur_quartier = Color.red;
+					}
+					polygone = new geoexplorer.gui.Polygon(Color.black, couleur_quartier);
+					
+					PGgeometry geom = (PGgeometry)res.getObject(2);
+					MultiPolygon pol = (MultiPolygon)geom.getGeometry();
+					for( int p = 0; p < pol.numPoints(); p++ )
+					{
+						Point pt = pol.getPoint(p);
+						polygone.addPoint(new geoexplorer.gui.Point(pt.x, pt.y));
+					}
+					mp.addPrimitive(polygone);
+
+
+				}
+				res.close();
+				
+				stmt = conn
+						.prepareStatement("SELECT ST_Transform(linestring, 2154) FROM ways WHERE tags->'highway' LIKE '%' AND ST_Intersects(linestring, ST_Setsrid(ST_GeomFromText('Polygon((5.8 45.1, 5.8 45.2, 5.7 45.2, 5.7 45.1, 5.8 45.1))'),4326))");
+
+				res = stmt.executeQuery();
+				geoexplorer.gui.LineString linestring;
+				while (res.next()) {
+					linestring = new geoexplorer.gui.LineString(Color.darkGray);
+					
+					PGgeometry geom = (PGgeometry)res.getObject(1);
+					LineString ls = (LineString)geom.getGeometry();
+					for( int p = 0; p < ls.numPoints(); p++ )
+					{
+						Point pt = ls.getPoint(p);
+						linestring.addPoint(new geoexplorer.gui.Point(pt.x, pt.y));
+					}
+					mp.addPrimitive(linestring);
+
+
+				}
+				res.close();
+				
 				Utils.closeConnection();
 
 			} catch (SQLException e) {
 				Logger.getLogger(Utils.class.getName()).log(Level.SEVERE, null,
 						e);
 			}
-			
-			/*geoexplorer.gui.Point point = new geoexplorer.gui.Point(5.73644115, 45.18644215);
-			point.setShape(point.CROSS);
-			mp.addPrimitive(point);*/
+
 			new GeoMainFrame("MapPanel", mp);		
 			
 		} else {
